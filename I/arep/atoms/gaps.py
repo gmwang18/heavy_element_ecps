ed072112704a27ead94aad48311401cc6ccb7fb7
Gani Annaberdiyev <aannabe@ncsu.edu>
2021-04-29 19:20:01 -0400

    update

--- I/arep/atoms/gaps.py
+++ I/arep/atoms/gaps.py
@@ -3,11 +3,16 @@
 import pickle 
 import pandas as pd
 
-pps = ['UC','MDFSTU','CRENBL','SBKJC','LANL2','ccECP']
-pd.options.display.float_format = '{:,.4f}'.format
+###==================================================
+
+pps=['UC','MDFSTU','CRENBL','SBKJC','LANL2','ccECP']
+remove_index = [10]
+lmad_index = [2,3,6]
 
 ###==================================================
+
 toev = 27.211386245988
+pd.options.display.float_format = '{:,.4f}'.format
 
 df = pd.DataFrame()
 
@@ -19,8 +24,7 @@ for pp in pps:
 	df[pp] = ecp['CCSD'].values-ecp['CCSD'].values[0]
 
 ### Drop some undesired states:
-df = df.drop(index=[10])
-
+df = df.drop(index=remove_index)
 
 diffs = df.copy()*toev
 diffs = diffs[1:]  # Getting rid of ground state
@@ -30,12 +34,26 @@ diffs = diffs.sub(ae_gaps, axis=0)
 mad = diffs.copy().abs().mean()
 diffs.loc['MAD'] = mad
 
+lmad = diffs.copy().loc[lmad_index].abs().mean()
+diffs.loc['LMAD'] = lmad
+
 weight = ae_gaps.abs()
 wmad = diffs.copy().abs().div(weight, axis=0).mean()*100
 diffs.loc['WMAD'] = wmad
 
 diffs['AE'] = ae_gaps  # Revert back to AE gaps
 
-#print(diffs)
-print(diffs.to_latex())
+### Sorting everything except AE and UC
+ecp_sorted = diffs.iloc[:,2:].sort_values(by="MAD", ascending=False, axis=1)
+final_sorted = pd.concat([diffs.iloc[:,0:2], ecp_sorted], axis=1)
+#print(final_sorted)
+print(final_sorted.to_latex())
+
+### Write MADs for later analysis
+spectral = final_sorted.loc["MAD":"WMAD"]
+#spectral = spectral.rename(columns={"CRENBS":"CRENBL"}) # Specific to Bi only
+spectral = spectral.T
+spectral = spectral[1:] # Getting rid of AE
+#print(spectral)
+spectral.to_csv("I.csv", float_format="%.4f")
 

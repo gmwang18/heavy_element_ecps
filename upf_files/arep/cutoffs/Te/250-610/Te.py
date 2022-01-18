1b1afbbd03850d46868ba04258a2fc2288daf61a
bkincaid256 <bekincai@ncsu.edu>
2022-01-18 11:39:31 -0500

    added directory 'cutoffs' in upf directory with README on how to use data to find cutoffs.

---
+++ upf_files/arep/cutoffs/Te/250-610/Te.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python
+from nexus import settings,job,run_project
+from nexus import generate_physical_system
+from nexus import generate_pwscf
+from nexus import generate_pw2qmcpack
+from nexus import generate_qmcpack,loop,linear,vmc,dmc
+from nexus import bundle                                                                                                                                                                                                             
+from structure import *
+settings(
+    pseudo_dir    = 'pseudos',
+    results       = '',
+    runs          = 'runs',
+    status_only   = 0,
+    generate_only = 0,
+    sleep         = 5,      # In seconds
+    machine       = 'cori', # Use lowercase letters
+    account       = 'm2113',   # QMCPACK
+    user          = 'bkincaid'
+    )
+qmc_presub = '''
+module unload cray-libsci
+module load cray-hdf5/1.10.2.0
+module load gcc
+module list
+'''
+scf_presub = '''
+export HDF5_USE_FILE_LOCKING=FALSE
+module load espresso
+module unload cray-libsci
+module load cray-hdf5/1.10.2.0
+module load gcc
+module list
+'''
+system = generate_physical_system(
+    units     = 'A',
+    #DIRECT LATTICE VECTORS CARTESIAN COMPONENTS (ANGSTROM)
+    axes      = [
+                [12.0, 0.0, 0.0],
+                [0.0, 12.0, 0.0],
+                [0.0, 0.0, 12.0],
+                ],
+    elem      = ['Te'],  # positions below should correspond to this
+    # CARTESIAN COORDINATES - PRIMITIVE CELL
+    net_spin = 2,
+    pos       = [
+                [6.0, 6.0, 6.0],
+                ],
+    #tiling    = [[1, -1, 0], [1, 1, 0], [0, 0, 1]],
+    tiling    = (1,1,1),
+    use_prim  = True, # use SeeK-path library to identify prim cell
+    #add_kpath = True, # use SeeK-path library to get prim k-path (use this for only getting band structure)
+    Te        = 6,
+    kgrid  = (1,1,1),   # Use this if interested in particular k-point excitations (QMC grid)
+    kshift = (0.0,0.0,0.0),   # Use this if interested in particular k-point excitations
+    )
+sims = []
+#### For QE
+#NODES=4
+#PROCS_PERNODE=32
+#HT=1
+#PROCS=NODES*PROCS_PERNODE
+#NTHREADS=(32*HT/PROCS_PERNODE)
+scf_dirs = {}
+for cut in range(250,610,10):
+    scf_dirs[cut] = generate_pwscf(
+        identifier   = 'scf_%i' % cut,
+        path         = 'scf_%i' % cut,
+        job          = job(nodes=1, cpus_per_task=16, queue='regular', hours=0.5, constraint='haswell', app='pw.x', presub=scf_presub),
+        input_type   = 'generic',
+        calculation  = 'scf',
+        nspin        = 2,   # 1 is non-polarized calculation (default)
+        #noncolin     = True,
+        #lspinorb     = True,
+        input_dft    = 'pbe', 
+        #exx_fraction = 0.15,
+       #nqx1         = 1,
+       #nqx2         = 1,
+       #nqx3         = 1,
+       #ecutfock     = 60,
+        ecutwfc      = cut,   
+        #conv_thr     = 1e-7, 
+        #occupations  = 'fixed',
+        #smearing     = 'gaussian',
+        #degauss      = 0.0001,
+        #nosym        = True, # should be False if nscf is next
+        #nosym_evc    = True,
+        #noinv        = True,
+        wf_collect   = True, # should be False if nscf is next
+        verbosity    = 'high',  # verbosity must be set to high
+        #disk_io      = 'low',
+        system       = system,
+        nbnd         = 12,       # a sensible nbnd value can be given 
+        #kgrid        = (1,1,1),  # Must be high-density
+        #kshift       = (0,0,0),
+        tot_magnetization = 2,
+        start_mag    = {"Te":0.50},
+        pseudos      = ['Te.upf'], 
+        #max_seconds  = (0.5-0.05)*3600,
+        #restart_mode = 'restart',
+        )
+    sims.append(scf_dirs[cut])
+
+###### pw2qmcpack should use the same parallelization options (MPI tasks and k-pools) as the last scf/nscf calculation!
+#conv = generate_pw2qmcpack(
+#    identifier   = 'conv',
+#    path         = 'scf',
+#    job          = job(nodes=1, tasks_per_node=1, cpus_per_task=1, queue='debug', hours=0.50, constraint='knl', app=espresso_path+'pw2qmcpack.x', presub=scf_presub),
+#    write_psir   = False,
+#    dependencies = (scf,'orbitals'),
+#    )
+#sims.append(conv)
+#
+run_project(sims)
---
+++ upf_files/arep/cutoffs/template/analyze.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+nelectron=5
+pwd=`pwd`
+#echo $pwd
+cd 250-610/runs
+
+for d in */ ; do
+        #echo $d
+        name=$(echo $d | awk '{split($1, array, "_"); printf("scf_%03d/", array[2])}')
+        #echo $name
+        if [[ "$d" != "$name" ]]; then
+                mv $d $name
+                echo $name
+        fi
+done
+#ls
+
+for d in */ ; do
+	echo "$d" >> $pwd/temp1.dat
+	cd $d
+	grep '!    total energy' *.out >> $pwd/temp2.dat
+
+	cd ../
+done
+
+cd $pwd/1000/runs
+grep '!    total energy' */*.out >> $pwd/compare.dat
+
+cd $pwd
+paste temp1.dat temp2.dat > data.dat
+
+rm temp1.dat temp2.dat
+
+awk  '{print $1}' data.dat > xs.dat
+awk  '{print $6}' data.dat > ys.dat
+paste xs.dat ys.dat > energy.dat
+rm xs.dat ys.dat
+sed -i 's/\///g' energy.dat 
+sed -i 's/scf_//g' energy.dat
+
+awk '{print $1}' energy.dat > temp1.dat
+ref=`awk '{print $5}' compare.dat` 
+#echo $ref
+awk -v ref=$ref -v nelec=$nelectron '{printf("%.8f\n", ($2-ref)*13.6056980659/nelec)}' energy.dat  > temp2.dat
+
+paste temp1.dat temp2.dat > processed.dat
+
+rm temp1.dat temp2.dat data.dat compare.dat
+
+awk '$2 < 0.001' processed.dat > final.dat
+
+head -n 1 final.dat > output.dat
+rm final.dat
+
+awk '{print $1" Ry      "$2" eV/e"}' output.dat > result.out
+rm output.dat
